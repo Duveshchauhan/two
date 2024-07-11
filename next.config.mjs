d0b1d38074ac4a395767e2c7b5572ff7e49b8796
Duveshchauhan <124759471+Duveshchauhan@users.noreply.github.com>
2024-07-11 12:07:31 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+        BASE_URL: "https://bgsw-as-heybosch-p-eus-001.azurewebsites.net/api",    
+        STT_LOCALES: "en-IN"
+    }
+};
+
+export default nextConfig;
